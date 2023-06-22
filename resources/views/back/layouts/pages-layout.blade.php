879766a4cbb26382b61ccf24077ec0a6e11ddbb8
Walter Valentini <walval@outlook.it>
2023-06-22 15:12:02 +0200

    Created home page with sticky top navbar

---
+++ resources/views/back/layouts/pages-layout.blade.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<!--
+* Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
+* @version 1.0.0-beta19
+* @link https://tabler.io
+* Copyright 2018-2023 The Tabler Authors
+* Copyright 2018-2023 codecalm.net Paweł Kuna
+* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
+-->
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>@yield('pageTitle')</title>
+    <!-- CSS files -->
+    <base href="/">
+    <link href="{{ asset('back/dist/css/tabler.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('back/dist/css/tabler-flags.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('back/dist/css/tabler-payments.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('back/dist/css/tabler-vendors.min.css') }}" rel="stylesheet" />
+    @stack('stylesheets')
+    <link href="{{ asset('back/dist/css/demo.min.css') }}" rel="stylesheet" />
+    <style>
+        @import url('https://rsms.me/inter/inter.css');
+
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
+
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
+    </style>
+</head>
+
+<body>
+    <script src="{{ asset('./back/dist/js/demo-theme.min.js') }}"></script>
+    <div class="page">
+       @include('back.layouts.inc.header')
+
+        <div class="page-wrapper">
+            <!-- Page header -->
+            @yield('pageHeader')
+            <!-- Page body -->
+            <div class="page-body">
+                <div class="container-xl">
+                    @yield('content')
+                </div>
+            </div>
+            @include('back.layouts.inc.footer')
+        </div>
+    </div>
+
+    <!-- Libs JS -->
+    <script src="{{ asset('./back/dist/libs/apexcharts/dist/apexcharts.min.js') }}" defer></script>
+    <script src="{{ asset('./back/dist/libs/jsvectormap/dist/js/jsvectormap.min.js') }}" defer></script>
+    <script src="{{ asset('./back/dist/libs/jsvectormap/dist/maps/world.js') }}" defer></script>
+    <script src="{{ asset('./back/dist/libs/jsvectormap/dist/maps/world-merc.js') }}" defer></script>
+    <!-- Tabler Core -->
+    <script src="{{ asset('./back/dist/js/tabler.min.js') }}" defer></script>
+    @stack('scripts')
+    <script src="{{ asset('./back/dist/js/demo.min.js') }}" defer></script>
+
+</body>
+
+</html>
--- resources/views/back/pages/home.blade.php
+++ resources/views/back/pages/home.blade.php
@@ -1,2 +1,104 @@
+@extends('back.layouts.pages-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Home')
+@section('content')
 
-Home Page 
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti dolor ipsum aut natus sunt doloremque? Consectetur
+    deleniti voluptatibus vel modi sequi saepe voluptatem iusto, ipsum ullam sint, aliquam quibusdam eaque!
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/32/2023/01/Guide-to-cyanotype-printing-b5a727f.jpg" class="card-img-top" alt="Card Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Card content goes here.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 my-2">
+                <div class="card">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/32/2023/01/Guide-to-cyanotype-printing-b5a727f.jpg" class="card-img-top" alt="Card Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Card content goes here.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 my-2">
+                <div class="card">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/32/2023/01/Guide-to-cyanotype-printing-b5a727f.jpg" class="card-img-top" alt="Card Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Card content goes here.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 my-2">
+                <div class="card">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/32/2023/01/Guide-to-cyanotype-printing-b5a727f.jpg" class="card-img-top" alt="Card Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Card content goes here.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 my-2">
+                <div class="card">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/32/2023/01/Guide-to-cyanotype-printing-b5a727f.jpg" class="card-img-top" alt="Card Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Card content goes here.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 my-2">
+                <div class="card">
+                    <img src="https://images.immediate.co.uk/production/volatile/sites/32/2023/01/Guide-to-cyanotype-printing-b5a727f.jpg" class="card-img-top" alt="Card Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Card content goes here.</p>
+                    </div>
+                </div>
+            </div>
+            
+
+         
+
+        </div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti dolor ipsum aut natus sunt doloremque?
+        Consectetur
+        deleniti voluptatibus vel modi sequi saepe voluptatem iusto, ipsum ullam sint, aliquam quibusdam eaque!
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti dolor ipsum aut natus sunt doloremque?
+        Consectetur
+        deleniti voluptatibus vel modi sequi saepe voluptatem iusto, ipsum ullam sint, aliquam quibusdam eaque!
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti dolor ipsum aut natus sunt doloremque?
+        Consectetur
+        deleniti voluptatibus vel modi sequi saepe voluptatem iusto, ipsum ullam sint, aliquam quibusdam eaque!
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti dolor ipsum aut natus sunt doloremque?
+        Consectetur
+        deleniti voluptatibus vel modi sequi saepe voluptatem iusto, ipsum ullam sint, aliquam quibusdam eaque!
+        <div class="accordion" id="accordionExample">
+            <div class="card">
+                <div class="card-header" id="headingOne">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
+                            aria-expanded="true" aria-controls="collapseOne">
+                            Accordion Item #1
+                        </button>
+                    </h2>
+                </div>
+
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                        Content for Accordion Item #1 goes here.
+                    </div>
+                </div>
+            </div>
+
+            <!-- Repeat the above accordion structure for additional items -->
+
+        </div>
+    </div>
+
+@endsection
