78ebb0cb11d0789637f09b6d217d062e45bf035f
Walter Valentini <walval@outlook.it>
2023-06-22 12:08:21 +0200

    added BS cdn but Tabler style still do not apply

--- resources/views/back/layouts/auth-layout.blade.php
+++ resources/views/back/layouts/auth-layout.blade.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <!--
 * Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
@@ -9,27 +8,30 @@
 * Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
 -->
 <html lang="en">
-  <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title> @yield('pageTitle') </title>
     <!-- CSS files -->
-    <link href="./back/dist/css/tabler.min.css" rel="stylesheet"/>
-    <link href="./back/dist/css/tabler-flags.min.css" rel="stylesheet"/>
-    <link href="./back/dist/css/tabler-payments.min.css" rel="stylesheet"/>
-    <link href="./back/dist/css/tabler-vendors.min.css" rel="stylesheet"/>
-    @stack('stylesheet')
-    <link href="./back/dist/css/demo.min.css" rel="stylesheet"/>
-    
-  </head>
-  <body  class=" d-flex flex-column">
-    <script src="./back/dist/js/demo-theme.min.js"></script>
-   @yield('content')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./back/dist/css/tabler.min.css" rel="stylesheet" />
+    <link href="./back/dist/css/tabler-flags.min.css" rel="stylesheet" />
+    <link href="./back/dist/css/tabler-payments.min.css" rel="stylesheet" />
+    <link href="./back/dist/css/tabler-vendors.min.css" rel="stylesheet" />
+    @stack('stylesheets')
+    <link href="./back/dist/css/demo.min.css" rel="stylesheets" />
+
+</head>
+
+<body class="  d-flex flex-column">
+    @yield('content')
     <!-- Libs JS -->
     <!-- Tabler Core -->
     <script src="./back/dist/js/tabler.min.js" defer></script>
     @stack('scripts')
     <script src="./back/dist/js/demo.min.js" defer></script>
-  </body>
+</body>
+
 </html>
