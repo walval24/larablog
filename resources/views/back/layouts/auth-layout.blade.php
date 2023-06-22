1d2019281e8c5401982d273cca812bc004d4d6e6
Walter Valentini <walval@outlook.it>
2023-06-22 11:42:06 +0200

    added tabler dist & static  to try fix css issue

--- resources/views/back/layouts/auth-layout.blade.php
+++ resources/views/back/layouts/auth-layout.blade.php
@@ -15,29 +15,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <title> @yield('pageTitle') </title>
     <!-- CSS files -->
-    <link href="./back/dist/css/tabler.min.css?1684106062" rel="stylesheet"/>
-    <link href="./back/dist/css/tabler-flags.min.css?1684106062" rel="stylesheet"/>
-    <link href="./back/dist/css/tabler-payments.min.css?1684106062" rel="stylesheet"/>
-    <link href="./back/dist/css/tabler-vendors.min.css?1684106062" rel="stylesheet"/>
+    <link href="./back/dist/css/tabler.min.css" rel="stylesheet"/>
+    <link href="./back/dist/css/tabler-flags.min.css" rel="stylesheet"/>
+    <link href="./back/dist/css/tabler-payments.min.css" rel="stylesheet"/>
+    <link href="./back/dist/css/tabler-vendors.min.css" rel="stylesheet"/>
     @stack('stylesheet')
-    <link href="./back/dist/css/demo.min.css?1684106062" rel="stylesheet"/>
-    <style>
-      @import url('https://rsms.me/inter/inter.css');
-      :root {
-      	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
-      }
-      body {
-      	font-feature-settings: "cv03", "cv04", "cv11";
-      }
-    </style>
+    <link href="./back/dist/css/demo.min.css" rel="stylesheet"/>
+    
   </head>
   <body  class=" d-flex flex-column">
-    <script src="./dist/js/demo-theme.min.js?1684106062"></script>
+    <script src="./back/dist/js/demo-theme.min.js"></script>
    @yield('content')
     <!-- Libs JS -->
     <!-- Tabler Core -->
-    <script src="./back/dist/js/tabler.min.js?1684106062" defer></script>
+    <script src="./back/dist/js/tabler.min.js" defer></script>
     @stack('scripts')
-    <script src="./back/dist/js/demo.min.js?1684106062" defer></script>
+    <script src="./back/dist/js/demo.min.js" defer></script>
   </body>
 </html>
