4d7c73a0d176b0a8609d5c7fc931fa8440b575ba
Walter Valentini <walval@outlook.it>
2023-06-22 13:57:00 +0200

    fixed the tabler css with "asset" tag

--- resources/views/back/layouts/auth-layout.blade.php
+++ resources/views/back/layouts/auth-layout.blade.php
@@ -15,13 +15,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title> @yield('pageTitle') </title>
     <!-- CSS files -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="./back/dist/css/tabler.min.css" rel="stylesheet" />
-    <link href="./back/dist/css/tabler-flags.min.css" rel="stylesheet" />
-    <link href="./back/dist/css/tabler-payments.min.css" rel="stylesheet" />
-    <link href="./back/dist/css/tabler-vendors.min.css" rel="stylesheet" />
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" /> -->
+
+    <link href="{{ asset('back/dist/css/tabler.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('back/dist/css/tabler-flags.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('back/dist/css/tabler-payments.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('back/dist/css/tabler-vendors.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('back/dist/css/demo.min.css') }}" rel="stylesheet" />
+
     @stack('stylesheets')
-    <link href="./back/dist/css/demo.min.css" rel="stylesheets" />
+
 
 </head>
 
@@ -29,9 +32,11 @@
     @yield('content')
     <!-- Libs JS -->
     <!-- Tabler Core -->
-    <script src="./back/dist/js/tabler.min.js" defer></script>
+    <script src="{{ asset('back/dist/js/tabler.min.js') }}" defer></script>
+    <script src="{{ asset('back/dist/js/demo.min.js') }}" defer></script>
+
     @stack('scripts')
-    <script src="./back/dist/js/demo.min.js" defer></script>
+   
 </body>
 
 </html>
