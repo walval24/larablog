2091667ff3da6d4d56c351c5561dbbda87bf2c7b
Walter Valentini <walval@outlook.it>
2023-06-25 17:43:46 +0200

    implemented login check

--- app/Http/Livewire/AuthorLoginForm.php
+++ app/Http/Livewire/AuthorLoginForm.php
@@ -3,9 +3,40 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthorLoginForm extends Component
 {
+    public $email, $password;
+
+    public function LoginHandler()
+    {
+        $this->validate([
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required|min:5'
+        ], [
+            'email.required' => 'Enter your email address',
+            'email.email' => 'Invalid email address',
+            'email.exists' => 'This email is not registered in database',
+            'password.required' => 'Password is required'
+        ]);
+
+        $creds = array('email' => $this->email, 'password' => $this->password);
+
+        if( Auth::guard('web')->attempt($creds)){
+            $checkUser = User::where('email', $this->email)->first();
+            if($checkUser->blocked == 1){
+                    Auth::guard('web')->logout();
+                    return redirect()->route('author.login')->with('fail','Your account had been blocked');
+
+            }else{
+                return redirect()-> route('author.home');
+            }
+        }
+
+
+    }
     public function render()
     {
         return view('livewire.author-login-form');
--- resources/views/back/pages/auth/login.blade.php
+++ resources/views/back/pages/auth/login.blade.php
@@ -8,40 +8,8 @@
         <div class="text-center mb-4">
           <a href="." class="navbar-brand navbar-brand-autodark"><img  src="https://static.wixstatic.com/media/4ab240_e80099a71ed2439cb7275199b615f140~mv2.png/v1/fill/w_128,h_136,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Untitled-1_edited.png" height="36" alt=""></a>
         </div>
-        <div class="card card-md">
-          <div class="card-body">
-            <h2 class="h2 text-center mb-4">Login to your account</h2>
-            <form action="./" method="get" autocomplete="off" novalidate="">
-              <div class="mb-3">
-                <label class="form-label">Email address</label>
-                <input type="email" class="form-control" placeholder="your@email.com" autocomplete="off">
-              </div>
-              <div class="mb-2">
-                <label class="form-label">
-                  Password
-                  <span class="form-label-description">
-                    <a href="{{route('author.forgot-password')}}">I forgot password</a>
-                  </span>
-                </label>
-                <div class="input-group input-group-flat">
-                  <input type="password" class="form-control" placeholder="Your password" autocomplete="off">
-                  <span class="input-group-text">
-                    <a href="#" class="link-secondary" data-bs-toggle="tooltip" aria-label="Show password" data-bs-original-title="Show password"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path></svg>
-                    </a>
-                  </span>
-                </div>
-              </div>
-              <div class="mb-2">
-                <label class="form-check">
-                  <input type="checkbox" class="form-check-input">
-                  <span class="form-check-label">Remember me on this device</span>
-                </label>
-              </div>
-              <div class="form-footer">
-                <button type="submit" class="btn btn-primary w-100">Sign in</button>
-              </div>
-            </form>
+        @livewire('author-login-form');
+
           </div>
       </div>
     </div>
