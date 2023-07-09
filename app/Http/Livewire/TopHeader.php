7d11789d70f6932f1a9292cfa0c2b985f3769472
Walter Valentini <walval@outlook.it>
2023-07-09 17:54:02 +0200

    Started working on profile user page

---
+++ app/Http/Livewire/TopHeader.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\User;
+
+class TopHeader extends Component
+{
+    public $author;
+
+    public function mount(){
+        $this->author = User::find(auth('web')->id()); 
+    }
+    public function render()
+    {
+        return view('livewire.top-header');
+    }
+}
--- resources/views/back/pages/profile.blade.php
+++ resources/views/back/pages/profile.blade.php
@@ -1,45 +1,90 @@
 @extends('back.layouts.pages-layout')
 @section('pageTitle', isset($pageTitle) ? $pageTitle : 'Profile')
 @section('content')
-    
-<div class="page-header">
-    <div class="row align-items-center">
-      <div class="col-auto">
-        <span class="avatar avatar-md" style="background-image: url(/samples/avatars/023m.jpg)"></span>
-      </div>
-      <div class="col-md-6">
-        <h2 class="page-title">Paweł Kuna</h2>
-        <div class="page-subtitle">
-          <div class="row">
+
+    <div class="page-header">
+        <div class="row align-items-center">
             <div class="col-auto">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <line x1="3" y1="21" x2="21" y2="21" />
-                <path d="M5 21v-14l8 -4v18" />
-                <path d="M19 21v-10l-6 -4" />
-                <line x1="9" y1="9" x2="9" y2="9.01" />
-                <line x1="9" y1="12" x2="9" y2="12.01" />
-                <line x1="9" y1="15" x2="9" y2="15.01" />
-                <line x1="9" y1="18" x2="9" y2="18.01" />
-              </svg>
-              <a href="#" class="text-reset">UI Designer at Tabler</a>
+                <span class="avatar avatar-md" style="background-image: url(/samples/avatars/023m.jpg)"></span>
             </div>
-          </div>
+            <div class="col-md-6">
+                <h2 class="page-title">Paweł Kuna</h2>
+                <div class="page-subtitle">
+                    <div class="row">
+                        <div class="col-auto">
+                            <a href="#" class="text-reset">UI Designer at Tabler</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto d-md-flex">
+                <a href="#" class="btn btn-primary">
+
+                    Edit Picture
+                </a>
+            </div>
         </div>
-      </div>
-      <div class="col-auto d-md-flex">
-        <a href="#" class="btn btn-primary">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M4 21v-13a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v6a3 3 0 0 1 -3 3h-9l-4 4" />
-            <line x1="8" y1="9" x2="16" y2="9" />
-            <line x1="8" y1="13" x2="14" y2="13" />
-          </svg>
-          Edit Picture
-        </a>
-      </div>
     </div>
-  </div>
-  
-    
+    <hr>
+    <div class="row">
+        <div class="card">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs" data-bs-toggle="tabs">
+                    <li class="nav-item">
+                        <a href="#tabs-details" class="nav-link active" data-bs-toggle="tab">Personal info</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#tabs-password" class="nav-link" data-bs-toggle="tab">Change Password</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-body">
+                <div class="tab-content">
+                    <div class="tab-pane active show" id="tabs-details">
+                        <h4>Home tab</h4>
+                        <div>
+                            <form method="post">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="mb-3">
+                                            <label class="form-label">Name</label>
+                                            <input type="text" class="form-control" name="example-text-input"
+                                                placeholder="Name">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-3">
+                                            <label class="form-label">Username</label>
+                                            <input type="text" class="form-control" name="example-text-input"
+                                                placeholder="UserName">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="mb-3">
+                                            <label class="form-label">Email</label>
+                                            <input type="email" class="form-control" name="example-text-input"
+                                                placeholder="Email" disabled>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Biography</label>
+                                    <textarea class="form-control" name="example-textarea-input" rows="6" placeholder="Content..">Biography...</textarea>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Save Changes</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="tab-pane" id="tabs-password">
+                        <h4>Profile tab</h4>
+                        <div>Fringilla egestas nunc quis tellus diam rhoncus ultricies tristique enim at diam, sem nunc
+                            amet, pellentesque id egestas velit sed</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
 @endsection
